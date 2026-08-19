13cafa68aaec05f18fb0a7694ad57ac3259c3038
Kamanzi Tony <kamanzitony06@gmail.com>
2026-08-19 19:55:59 +0200

    feat: add user model and authentication routes, update package.json author and module type

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+// user auth schema
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+
+  lastName: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+}, {
+    timestamps: true
+});
+
+
+const User = mongoose.model("User", userSchema)
+
+
+export default User
