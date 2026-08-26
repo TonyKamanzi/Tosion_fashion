2403cf9609e76608da12d0d7f23ed48a11f005fc
Kamanzi Tony <kamanzitony06@gmail.com>
2026-08-26 19:20:52 +0200

    feat: wrap layout with WishlistProvider

--- frontend/app/(site)/layout.tsx
+++ frontend/app/(site)/layout.tsx
@@ -1,15 +1,18 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { CartProvider } from "@/components/shop/CartContext";
+import { WishlistProvider } from "@/components/shop/WishlistContext";
 
 export default function SiteLayout({ children }: { children: React.ReactNode }) {
     return (
         <CartProvider>
-            <div className="flex flex-col flex-1">
-                <Header />
-                {children}
-                <Footer />
-            </div>
+            <WishlistProvider>
+                <div className="flex flex-col flex-1">
+                    <Header />
+                    {children}
+                    <Footer />
+                </div>
+            </WishlistProvider>
         </CartProvider>
     );
 }
